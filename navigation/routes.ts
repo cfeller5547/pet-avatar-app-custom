559a661716c1843aea0ecdc13b27aeff0da8f0cc
chris <cfeller@seamgen.com>
2025-05-23 15:39:06 -0700

    continue setting up app skeleton

---
+++ navigation/routes.ts
@@ -0,0 +1,7 @@
+export const routes = {
+  PROFILE:    '/(onboarding)/profile',
+  PROCESSING: '/(onboarding)/processing',
+  AVATAR:     '/(main)/pet',    
+  GALLERY:    '/(main)/gallery',
+  SETTINGS:   '/(main)/settings',
+} as const;
